732db1915a251fa419a6bf4eab99bda0d6d02b9a
chilichen0118 <105632629+chilichen0118@users.noreply.github.com>
2023-04-16 02:06:18 +0800

    Add files via upload

--- main.js
+++
@@ -1,8 +0,0 @@
-$(window).on('load', function(){
-    // $('.loading').removeClass('d-flex');
-    // $('.loading').addClass('d-none');
-    setTimeout(function() {
-        $('.loading').fadeOut();
-        $(window).scrollTop(0);
-    }, 2000);
-});
