3f12b943eaa5a1a084944995fb29d4a1a1b3af09
chilichen0118 <105632629+chilichen0118@users.noreply.github.com>
2023-04-16 12:57:09 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -39,4 +39,27 @@ $('.owl-carousel').owlCarousel({
             items:5
         }
     }
+});
+
+$(function () {
+    cursorX = 0;
+    cursorY = 0;
+    mouseX = 0;
+    mouseY = 0;
+    $(window).on('mousemove', function (e) {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+    });
+    setInterval(function () {
+        cursorX += (mouseX - cursorX) / 20; //後面的數字愈小動愈快
+        cursorY += (mouseY - cursorY) / 20;
+        $('.cursor').css({
+            'top': cursorY + 'px',
+            'left': cursorX + 'px',
+        })
+    }, 10) //每幾毫秒更新一次，數字愈小愈流暢
+});
+
+$('.menu-btn').on('click', function(){
+    $('.menu').toggleClass('active');
 })
